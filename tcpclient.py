6ea01b50d5c85515f3d137d3ffc4daa739879920
hardrock1337 <hardrock1338@gmail.com>
2020-06-30 00:16:04 +0700

    2020-06-30

--- tcpclient.py
+++ tcpclient.py
@@ -1,7 +1,7 @@
 import socket
 
 
-TCP_IP = '192.168.11.92'
+TCP_IP = '192.168.11.91'
 TCP_PORT = 1500
 BUFFER_SIZE = 4096
 j = 0
@@ -27,7 +27,7 @@ shearer_motor_status_m4 = 0     # Reg[15], Sstat, 0-15
 shearer_motor_current_m5 = 0    # Reg[16,17], uint, 0-1000 Amper
 shearer_motor_status_m5 = 0     # Reg[18], Sstat, 0-15
 shearer_speed = 0.0             # Reg[19,20], uint, 0-250, 0.1m/min 20 registr empty?
-shearer_status = 0              # Reg[21], Sstat, 0-15
+shearer_speed_status = 0        # Reg[21], Sstat, 0-15
 shearer_current_year = ""       # Reg[22], uchar, 0-99 year
 shearer_current_mounth = ""     # Reg[23], uchar, 0-12 mounth
 shearer_current_day = ""        # Reg[24], uchar, 1-31 day
@@ -39,7 +39,7 @@ shearer_management_command_panel = 0    # Reg[29], uchar, 0-SpeedUP, 1-SpeedDown
 shearer_electro_hydro_valve = 0 # Reg[30], uchar
 shearer_management_drobilka = 0 # Reg[31], uchar
 shearer_sensor_speed = 0        # Reg[32], uchar
-shearer_sensor_pologenie = 0    # Reg[32], uchar
+shearer_sensor_pologenie = 0    # Reg[33], uchar
 shearer_operation_mode = 0      # Reg[34], uchar
 shearer_type = 0                # Reg[35], uchar
 shearer_reason_off = 0          # Reg[36], uchar
@@ -54,16 +54,16 @@ shearer_speed_regulation_status = 0 # Reg[54], Sstat
 shearer_postion_number_section = 0  # Reg[55], uchar, 0 - 250
 shearer_postion_number_section_status= 0 # Reg[56], Sstat
 shearer_voltage = 0             # Reg[57,58], uint, 0 - 1500
-shearer_level_defence_m1 = 0    # 0 - 250
-shearer_level_defence_status_m1 = 0
-shearer_level_defence_m2 = 0    # 0 - 250
-shearer_level_defence_status_m2 = 0
-shearer_level_defence_m3 = 0    # 0 - 250
-shearer_level_defence_status_m3 = 0
-shearer_level_defence_m4 = 0    # 0 - 250
-shearer_level_defence_status_m4 = 0
-shearer_level_defence_m5 = 0    # 0 - 250
-shearer_level_defence_status_m5 = 0
+shearer_level_defence_m1 = 0    # Reg[76], uchar, 0 - 250
+shearer_level_defence_status_m1 = 0 # Reg[77], Sstat
+shearer_level_defence_m2 = 0    # Reg[78], uchar, 0 - 250
+shearer_level_defence_status_m2 = 0 # Reg[79], Sstat
+shearer_level_defence_m3 = 0    # Reg[80], uchar, 0 - 250
+shearer_level_defence_status_m3 = 0 # Reg[81], Sstat
+shearer_level_defence_m4 = 0    # Reg[82], uchar, 0 - 250
+shearer_level_defence_status_m4 = 0 # Reg[83], Sstat
+shearer_level_defence_m5 = 0    # Reg[84], uchar, 0 - 250
+shearer_level_defence_status_m5 = 0 # Reg[85], Sstat
 shearer_serial_number = 0    # 0 - 99, 000 - 999
 
 def mprint():
@@ -80,41 +80,48 @@ def parser_shearer():
     """TO DO"""
     mprint()
     print("shearer_data_status = ",data[17])
-    print("shearer_software_version = ",str(mstr[1][2]) + "." + str(mstr[1][3]))
-    print("shearer_motor_current_m1 = ",mstr[1][4] + mstr[1][5])
-    print("shearer_motor_status_m1 = ",mstr[1][6])
-    print("shearer_motor_current_m2 = ",mstr[2][0] + mstr[2][1])
-    print("shearer_motor_status_m2 = ", mstr[2][2])
-    print("shearer_motor_current_m3 = ", mstr[2][3] + mstr[2][4])
-    print("shearer_motor_status_m3 = ", mstr[2][5])
-    print("shearer_motor_current_m4 = ", mstr[2][6] + mstr[3][0])
-    print("shearer_motor_status_m4 = ", mstr[3][1])
-    print("shearer_motor_current_m5 = ", mstr[3][2] + mstr[3][3])
-    print("shearer_motor_status_m5 = ", mstr[3][4])
-    print("shearer_speed = ", mstr[3][5] + mstr[3][6]/10)  # 0-250, 0.1m/min
-    print("shearer_management_rele = ", mstr[5][0])
-    print("shearer_management_command_panel = ",mstr[5][1])
-    print("shearer_electro_hydro_valve = ", mstr[5][2])
-    print("shearer_management_drobilka = ", mstr[5][3])
-    print("shearer_sensor_speed = ", mstr[5][4])
-    print("shearer_operation_mode = ", mstr[5][6])
-    print("shearer_type = ", mstr[6][0])
-    print("shearer_reason_off = ",  mstr[6][1])
-    print("shearer_traversed_path = ",  mstr[6][3] +  mstr[6][4] +  mstr[6][5] +  mstr[6][6])
-    print("shearer_postion = ", mstr[7][2])
-    print("shearer_time_work = ",  mstr[8][0] + mstr[8][1] + mstr[8][2] + mstr[8][3])
-    print("shearer_speed_regulation = ", mstr[8][4]/10)  # 0 - 150, 0.1m/min
-    print("shearer_postion_number_section = ", mstr[8][6])  # 0 - 250
-    print("shearer_voltage = ", str(mstr[9][0]) + str(mstr[9][1]))  # 0 - 1500
-    print("shearer_level_defence_m1 = ", mstr[11][5])  # 0 - 250
-    print("shearer_level_defence_status_m1 = ", mstr[11][6])
-    print("shearer_level_defence_m2 = ", mstr[12][0]) # 0 - 250
-    print("shearer_level_defence_status_m2 = ", mstr[12][1])
-    print("shearer_level_defence_m3 = ", mstr[12][2])
-    print("shearer_level_defence_status_m3 = ", mstr[12][3])
-    print("shearer_level_defence_m4 = ", mstr[12][4])
-    print("shearer_level_defence_status_m4 = ", mstr[12][5])
-    print("shearer_level_defence_m5 = ", mstr[12][6])
+    print("shearer_software_version = ",str(data[19]) + "." + str(data[20]))
+    print("shearer_motor_current_m1 = ",str(data[21]) + str(data[22]))
+    print("shearer_motor_status_m1 = ",data[23])
+    print("shearer_motor_current_m2 = ",str(data[29]) + str(data[30]))
+    print("shearer_motor_status_m2 = ", data[31])
+    print("shearer_motor_current_m3 = ", str(data[32]) + str(data[33]))
+    print("shearer_motor_status_m3 = ", data[34])
+    print("shearer_motor_current_m4 = ", str(data[35]) + str(data[41]))
+    print("shearer_motor_status_m4 = ", data[42])
+    print("shearer_motor_current_m5 = ", str(data[43]) + str(data[44]))
+    print("shearer_motor_status_m5 = ", data[45])
+    print("shearer_speed = ", str(data[46]) + str(data[47]))  # 0-250, 0.1m/min
+    print("shearer_speed_status = ", data[53])
+    print("shearer_current_year = ", data[54])
+    print("shearer_current_mounth = ", data[55])
+    print("shearer_current_day = ", data[56])
+    print("shearer_current_hour = ", data[57])
+    print("shearer_current_minute = ", data[58])
+    print("shearer_current_sec = ", data[59])
+    print("shearer_management_rele = ", data[65])
+    print("shearer_management_command_panel = ", data[66])
+    print("shearer_electro_hydro_valve = ", data[67])
+    print("shearer_management_drobilka = ", data[68])
+    print("shearer_sensor_speed = ", data[69])
+    print("shearer_operation_mode = ", data[70])
+#    print("shearer_type = ", data)
+#    print("shearer_reason_off = ",  mstr[6][1])
+#    print("shearer_traversed_path = ",  mstr[6][3] +  mstr[6][4] +  mstr[6][5] +  mstr[6][6])
+#    print("shearer_postion = ", mstr[7][2])
+#    print("shearer_time_work = ",  mstr[8][0] + mstr[8][1] + mstr[8][2] + mstr[8][3])
+#    print("shearer_speed_regulation = ", mstr[8][4]/10)  # 0 - 150, 0.1m/min
+#    print("shearer_postion_number_section = ", mstr[8][6])  # 0 - 250
+#    print("shearer_voltage = ", str(mstr[9][0]) + str(mstr[9][1]))  # 0 - 1500
+#    print("shearer_level_defence_m1 = ", mstr[11][5])  # 0 - 250
+#    print("shearer_level_defence_status_m1 = ", mstr[11][6])
+#    print("shearer_level_defence_m2 = ", mstr[12][0]) # 0 - 250
+#    print("shearer_level_defence_status_m2 = ", mstr[12][1])
+#    print("shearer_level_defence_m3 = ", mstr[12][2])
+#    print("shearer_level_defence_status_m3 = ", mstr[12][3])
+#    print("shearer_level_defence_m4 = ", mstr[12][4])
+#    print("shearer_level_defence_status_m4 = ", mstr[12][5])
+#    print("shearer_level_defence_m5 = ", mstr[12][6])
     #print("shearer_level_defence_status_m5 = ", mstr[13][0])
     #print("shearer_serial_number = ", mstr[13][1] + " " + mstr[13][2])
 
@@ -124,10 +131,8 @@ def parser_frequence():
 def head_check():
     """From message andsudo """
     if data[9] == 16 and data[10] == 16:
-        mprint()
         parser_shearer()
     elif data[9] == 18 and data[10] == 18:
-        mprint()
         parser_frequence()
     else:
         mprint()
